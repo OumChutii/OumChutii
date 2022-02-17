446509ade416a021dc112af2731bffda131364d0
chutikan prasert <chutikan.prasert@gmail.com>
2022-02-17 14:16:20 +0700

    add kumkom

--- intro-to-js/chutikan.js
+++ intro-to-js/chutikan.js
@@ -2,11 +2,11 @@ function askMyName() {
     const name = prompt("Enter your name")
  
     if (name != null) {
-       document.getElementById("name").innerHTML = "My name is " + name;
+       document.getElementById("name").innerHTML = "My name is " + "Oum";
     }
  }
  
 function helloWorld() {
     const h1message = document.getElementById('name').innerHTML;
-    alert(h1message + 'มือใหม่หัดขับ')
+    alert(h1message + 'ขับรถจี้ขนาดนี้ จะให้ขำเลยใช่ไหม')
  }
