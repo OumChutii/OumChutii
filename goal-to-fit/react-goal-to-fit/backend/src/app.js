3901247a88d583476d1e7e33e48feb0d991a98a0
chutikan prasert <chutikan.prasert@gmail.com>
2022-04-20 00:27:31 +0700

    update task 7

---
+++ goal-to-fit/react-goal-to-fit/backend/src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const usersRouter = require('./routes/users')
+const actRouter = require('./routes/activities')
+
+const app = express();
+
+// Body parse middleware
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended:false}));
+
+app.get('/', (req, res) => {
+    res.send(`Welcome to web server on port ${PORT}`);
+})
+
+app.use('/users', usersRouter)
+app.use('/activities', actRouter)
+
+
+const PORT = process.env.PORT || 4001;
+app.listen(PORT, () => console.log(`server is running  on port ${PORT}`));
